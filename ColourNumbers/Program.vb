6fbe9d0039820439d07c8297a6eb44c8375dd075
benstone24 <82447075+benstone24@users.noreply.github.com>
2021-09-10 23:06:07 +0100

    Add High number warning
    Added a high number warning system to warn users when entering a number >500000 to warn them that the number they have entered is not optimal for performance.

--- ColourNumbers/Program.vb
+++ ColourNumbers/Program.vb
@@ -9,8 +9,9 @@ Module Program
     Dim age As Integer  'Age of user 
     Dim feeling As String 'How the user feels
     Dim NumberCount As Integer 'The current number the program is counting 
-    Dim MaxNumber As Integer 'The maximum number the program will count up to
+    Dim MaxNumber As Integer 'The highest number the program will count up to
     Dim CurrentColour As Integer 'The current colour of the foreground 
+    Dim NumberConfirm As String
     
          Sub Main(args As String())
              WriteLine("Hello! What's your name?")
@@ -30,15 +31,35 @@ Module Program
              
              WriteLine("How high do you want me to count?")
              MaxNumber = ReadLine()
+            
+             '-------------------'
+             'HIGH NUMBER WARNING'
+             '-------------------'
              
-             For NumberCount = 1 To MaxNumber
-                 ForegroundColor = CurrentColour
-                 WriteLine(NumberCount)
-                 CurrentColour = CurrentColour + 1
-                 If CurrentColour = 15 Then 'If the current colour has reached the max (16) then set it back to 0 to avoid crashing 
-                     CurrentColour = 0
+             
+             If MaxNumber > 500000 Then 
+                 WriteLine("This number may be too large to process!!!")
+                 WriteLine("This may have negative effects on lower performing devices.")
+                 WriteLine("If you still wish to go ahead, please type 'y' (case sensitive) to continue.")
+                 NumberConfirm = ReadLine()
+                 If NumberConfirm = "y" Then 
+                     
+                     For NumberCount = 1 To MaxNumber
+                         ForegroundColor = CurrentColour
+                         WriteLine(NumberCount)
+                         CurrentColour = CurrentColour + 1
+                         If CurrentColour = 15 Then 'If the current colour has reached the max (16) then set it back to 0 to avoid crashing 
+                             CurrentColour = 0
+                             End If 
+                     Next
+                         
+                         Else
+                             End
+                         End If
+                      
                  End If
-             Next
-         WriteLine("Done!")
+             
+             
+             
          End Sub
 End Module
